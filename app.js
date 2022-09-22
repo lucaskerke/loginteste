8234bfbb0bdc620ec4cfe54ebefc5cefc7cff961
lucaskerke <lucaskerke21@gmail.com>
2022-09-22 13:45:28 -0300

    avanço

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const usuario = document.querySelector("#usuario");
 const senha = document.querySelector("#senha");
 const corpo = document.querySelector(".corpo");
+const h2username = document.querySelector(".h2username");
+const h2password = document.querySelector(".h2password");
 const usuario_id = document.getElementById('usuario').id;
 const senha_id = document.getElementById('senha').id;
 const usuario_value = document.getElementById('usuario').value;
@@ -10,10 +12,19 @@ const senha_value = document.getElementById('senha').value;
 function aumentaInput(){
     usuario.style.height = "20px";
     senha.style.height = "1px";
+    h2username.style.fontSize = "15px";
+    h2password.style.fontSize = "24px";
+    h2username.style.color = "rgb(28, 211, 211)";
+    h2password.style.color = "rgb(104, 104, 104)";
 }
 function aumentaInputSegundo(){
     senha.style.height = "20px";
     usuario.style.height = "1px";
+    h2username.style.fontSize = "24px";
+    h2password.style.fontSize = "15px";
+    h2username.style.color = "rgb(104, 104, 104)";
+    h2password.style.color = "rgb(28, 211, 211)";
+    
  }
 
 /*function consulta(){
